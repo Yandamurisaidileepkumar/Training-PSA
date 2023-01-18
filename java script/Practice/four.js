46a4026668fc8811bd0b9dcf118ec86e18af7511
Yandamuriisaidileepkumar <yandamuridileepkumar16@gmail.com>
2023-01-18 13:40:37 +0530

    okie

---
+++ java script/Practice/four.js
@@ -0,0 +1,12 @@
+/*let person ={
+    firstname:"dileep",
+    lastname :"yandamuri",
+    age:null
+}
+console.log(typeof(person.firstname))*/
+// console.log()
+//console.log(Boolean(NaN))
+let a=false
+let b= true
+console.log(a==b)
+console.log(a===b)
