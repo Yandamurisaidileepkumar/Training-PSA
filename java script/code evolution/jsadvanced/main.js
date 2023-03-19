4887d940b815adb3b65a7d39ba50de0e35ce6a8b
Yandamuriisaidileepkumar <yandamuridileepkumar16@gmail.com>
2023-03-19 22:12:55 +0530

    okie

--- java script/code evolution/jsadvanced/main.js
+++ java script/code evolution/jsadvanced/main.js
@@ -35,3 +35,52 @@
 // const fn = outer();
 // fn();
 // fn();
+
+// this keyword
+
+// function saymyname(name) {
+//   console.log(`my name is ${name}`);
+// }
+// saymyname("dileep");
+// saymyname("walter white");
+
+//implicit binding
+
+// const person = {
+//   name: "dileep",
+//   SayMyName: function () {
+//     console.log(`my name is ${this.name}`);
+//   },
+// };
+// person.SayMyName();
+
+// explicit binding
+// const person = {
+//   name: "yandamuri",
+// };
+// function SayMyName() {
+//   console.log(`my name is ${this.name}`);
+// }
+// SayMyName.call(person);
+
+// new binding
+
+// function person(name) {
+//   this.name = name;
+// }
+// const p1 = new person("dileep");
+// const p2 = new person("prem");
+// console.log(p1.name);
+// console.log(p2.name);
+
+// Default binding
+// function saymyname(name) {
+//     console.log(`my name is ${this.name}`);
+//   }
+//   saymyname();
+
+// name = "prem";
+// function saymyname(name) {
+//   console.log(`my name is ${this.name}`);
+// }
+// saymyname();
